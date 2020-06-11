d4724b6e0c115b3a7e4a5bd5e8ba49a111a098e9
alfinwilliam <alfinwilliam@gmail.com>
2020-06-11 19:51:00 +0530

    tweepy revamped

--- ipynb files/twitter_credentials.py
+++
@@ -1,6 +0,0 @@
-# YouTube Video: https://www.youtube.com/watch?v=wlnx-7cm4Gg
-# Variables that contains the user credentials to access Twitter API 
-ACCESS_TOKEN = "183906186-nOLz1Ivzed3AGycbaBAiTL7zE8vXkW7L3Lf06tsn"
-ACCESS_TOKEN_SECRET = "gRn4hJiyQJLx7N1nLDvzghFse3poWdvsMimBHRJaKlKXR"
-CONSUMER_KEY = "gIENuDUpWlld1sCNPGz5UrLYR"
-CONSUMER_SECRET = "uwRPNA1j4reQlxLw0LA6j8ha8CcofUXYtZkIHZE7rJOv0EtvpP"
