38f3f14689ade482a67dd5f5b2241ca693e8fe0b
alfinwilliam <alfinwilliam@gmail.com>
2020-08-04 14:51:29 +0530

    added gitignore

--- gui/sample.py
+++ gui/sample.py
@@ -1,23 +1,26 @@
 from PyQt5 import QtWidgets, uic
 import sys
-	
-	
-	
-	class Ui(QtWidgets.QMainWindow):
-	    def __init__(self):
-	        super(Ui, self).__init__()
-	        uic.loadUi('mainwindow.ui', self)
-	
-	        self.button = self.findChild(QtWidgets.QPushButton, 'pbStartVideoCapture')
-	        self.button.clicked.connect(self.openCaptureWindow)
-	
-	        self.exitButton = self.findChild(QtWidgets.QPushButton,'pbExit')
-	        self.exitButton.clicked.connect(self.close)
-	        self.show()
-	    
 
-	
-	
-	app = QtWidgets.QApplication(sys.argv)
-	window = Ui()
-	app.exec_()
+
+class Ui(QtWidgets.QMainWindow):
+
+    def procbtnclicked(self):
+        self.label = self.findchild(QtWidgets.QLabel, 'Label_2')
+        self.label.setText("Alfin")
+
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi('sma.ui', self)
+
+        self.procbtn = self.findChild(
+            QtWidgets.QPushButton, 'pbproceed')
+        self.procbtn.clicked.connect(self.procbtnclicked)
+
+        #self.exitButton = self.findChild(QtWidgets.QPushButton, 'pbExit')
+        # self.exitButton.clicked.connect(self.close)
+        self.show()
+
+
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+app.exec_()
